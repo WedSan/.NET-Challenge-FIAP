b7af3e705c50b1dcfcea8ae73788ed3632eaa37b
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-30 10:47:03 -0300

    feat: add class to represent Dental Procedure table in the DB

---
+++ Domain/DentalProcedure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DentalProcedure
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public DentalHistory DentalHistory { get; set; }
+
+        public DentalProcedure(int id, string name, DentalHistory dentalHistory)
+        {
+            Id = id;
+            Name = name;
+            DentalHistory = dentalHistory;
+        }
+    }
+}
