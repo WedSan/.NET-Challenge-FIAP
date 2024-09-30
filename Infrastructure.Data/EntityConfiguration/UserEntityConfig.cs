c7d3fbc4a6069ece036710dbd27c0ca34289e2ad
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-30 17:18:42 -0300

    feat: configure User entity mapping to database table TB_USUARIO

---
+++ Infrastructure.Data/EntityConfiguration/UserEntityConfig.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Infrastructure.EntityConfiguration
+{
+    public class UserEntityConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("TB_USUARIO");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                   .HasColumnName("ID");
+
+
+            builder.Property(e => e.Name)
+                .HasColumnName("NOME");
+
+
+            builder.Property(e => e.Email)
+                .HasColumnName("EMAIL");
+
+            builder.Property(e => e.Password)
+                .HasColumnName("SENHA");
+
+            builder.Property(e => e.Gender)
+                .HasColumnName("GENERO")
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Gender)Enum.Parse(typeof(Gender), v));
+
+        }
+
+    }
+}
