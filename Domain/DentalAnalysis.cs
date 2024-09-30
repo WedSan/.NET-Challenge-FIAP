01940efca64239420eb234cded7c89016e7875e1
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-30 10:46:31 -0300

    feat: add class to represent Dental Analysis table in the DB

---
+++ Domain/DentalAnalysis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DentalAnalysis
+    {
+        public int Id { get; init; }
+
+        public User user { get; init; }
+
+        public DateTime AnalysisDate { get; init; }
+
+        public float ProbabilityProblem { get; init; }
+
+        List<MonitoringData> MonitoringDataList { get; init; }
+
+        public DentalAnalysis(int id, User user, DateTime analysisDate, float probabilityProblem, List<MonitoringData> monitoringDataList)
+        {
+            Id = id;
+            this.user = user;
+            AnalysisDate = analysisDate;
+            ProbabilityProblem = probabilityProblem;
+            MonitoringDataList = monitoringDataList;
+        }
+    }
+}
