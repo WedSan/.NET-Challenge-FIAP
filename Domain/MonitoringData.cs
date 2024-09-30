468625a997b7b5ad18a1040cd3725caf67ec8047
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-30 10:49:51 -0300

    feat: add class to represent Report Dental Problem table in the DB

---
+++ Domain/MonitoringData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class MonitoringData
+    {
+
+        public int Id { get; init; }
+
+        public User User { get; init; }
+
+        public List<ReportDentalProblem> Problems { get; init; }
+
+        public List<DentalAnalysis> DentalAnalyses { get; init; }
+
+        public DateTime RegistrationDate { get; init; } 
+
+        public MonitoringData(int id, User user, List<ReportDentalProblem> problems, List<DentalAnalysis> dentalAnalyses, DateTime registrationDate)
+        {
+            Id = id;
+            this.User = user;
+            this.Problems = problems;
+            this.DentalAnalyses = dentalAnalyses;
+            this.RegistrationDate = registrationDate;
+        }
+    }
+}
---
+++ Domain/ReportDentalProblem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ReportDentalProblem
+    {
+        public int Id {  get; init; }
+
+        public string Problem { get; set; }
+
+        public ReportDentalProblem(int id, string problem)
+        {
+            Id = id;
+            this.Problem = problem;
+        }
+    }
+}
