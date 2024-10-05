c05e16a194fdb881ec033bddb2cbb26bb41a3d2b
WedSan <andre.alves.silvazesez@gmail.com>
2024-10-04 22:37:45 -0300

    feat: add interface to define how to implement monitoring data service

---
+++ Domain/Interfaces/Services/IMonitoringDataService.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services
+{
+    public interface IMonitoringDataService
+    {
+        Task<MonitoringData> CreateMonitoringDataAsync(int userId, List<ReportDentalProblem> problems);
+
+        Task<IEnumerable<MonitoringData>> GetMonitoringDataAsync(int pageNumber, int pageSize);
+
+        Task<MonitoringData> GetMonitoringDataByIdAsync(int monitoringDataId);
+
+        Task<MonitoringData> UpdateMonitoringDataAsync(int monitoringDataId, List<ReportDentalProblem> dentalProblems);
+
+        Task DeleteMonitoringDataAsync(int monitoringDataId);
+    }
+}
