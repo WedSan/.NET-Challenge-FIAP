a3963d18a2bbe197bddcf0ad616031a6e0c4cd55
WedSan <andre.alves.silvazesez@gmail.com>
2024-10-06 23:37:48 -0300

    feat: add controller to allow HTTP operations requests for monitoring data entity

---
+++ web/Controllers/MonitoringDataController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web.DTO.DataMonitoring;
+using web.Mapper;
+
+namespace web.Controllers
+{
+    [Controller]
+    [Route("api/v1/monitoring-data")]
+    public class MonitoringDataController : Controller
+    {
+        private IMonitoringDataService _service;
+
+        public MonitoringDataController(IMonitoringDataService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MonitoringDataResponse>> CreateMonitoringData([FromBody] AddDataMonitoringRequest request)
+        {
+            MonitoringData monitoringData = await _service.CreateMonitoringDataAsync(request.UserId, MonitoringDataMapper.MapToDentalProblem(request.DentalProblems));
+            MonitoringDataResponse response = MonitoringDataMapper.ToDto(monitoringData);
+            return CreatedAtAction(nameof(CreateMonitoringData), response); 
+        }
+    }
+}
