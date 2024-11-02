1fe5498da6c81654d49be02a5012e310e9a5c8ad
WedSan <andre.alves.silvazesez@gmail.com>
2024-11-01 23:31:59 -0300

    feat: add default constructor to allow build the object without args

--- web/DTO/DentalHistory/AddDentalHistoryRequest.cs
+++ web/DTO/DentalHistory/AddDentalHistoryRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace web.DTO.DentalHistory
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace web.DTO.DentalHistory
 {
     public record AddDentalHistoryRequest(
         int UserId,
         List<string> Procedures,
         DateTime ConsultationDate,
         string ToothCondition
-    );
+    )
+    {
+        public AddDentalHistoryRequest() : this(0, new List<string>(), DateTime.Now, string.Empty)
+        {
+        }
+    }
+        
+ 
 }
